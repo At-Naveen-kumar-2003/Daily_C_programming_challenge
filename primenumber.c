145d9f6588ec0fdbc5591668d3716aa593c07c08
Naveen kumar s <mailmeatnaveens@gmail.com>
2026-06-09 12:24:23 +0530

    prime numbers

---
+++ primenumber.c
@@ -0,0 +1,32 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int prime(int *num);
+
+int main()
+{
+    int num1;
+    printf("Enter any number:");
+    if(scanf("%d",&num1) !=1)
+    {
+        printf("Invalid input");
+        return 1;
+    }
+    printf("the number is prime %d",prime(&num1));
+    return 0;
+}
+int prime(int *num)
+{
+    int n = *num;
+    if(n<=1)return 0;
+    if(n<=3)return 1;
+    if(n%3==0 || n%2==0) return 0;
+    for(int i=5;i*i<n;i+=6)
+    {
+        if((n%i==0)||(n%i+2==0))
+        {
+            return 0;
+        }
+    }
+    return 1;
+}
