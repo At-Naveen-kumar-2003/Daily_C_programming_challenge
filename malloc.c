93d8dd43e66ead005a7020569b62a6057e446a99
Naveen kumar s <mailmeatnaveens@gmail.com>
2026-07-08 10:46:12 +0530

    malloc creation

---
+++ malloc.c
@@ -0,0 +1,36 @@
+#include<stdio.h>
+#include<stdlib.h>
+int main()
+{
+    int n,i;
+    int *arr;
+
+    printf("Enter the number:");
+    scanf("%d",&n);
+
+    arr = (int *)malloc(n*sizeof(int));
+    if(arr== NULL)
+    {
+        printf("Memory allocation is failed");
+        return 1;
+    }
+    printf("The Values initilized before (garbage values)");
+    for(int i=0;i<n;i++)
+    {
+        printf("arr[%d] = %d   \n",i,arr[i]);
+    }
+    printf("Enter the values :");
+    for(int i=0;i<n;i++)
+    {
+        scanf("%d",&arr[i]);
+    }
+    printf("The values are:");
+      for(int i=0;i<n;i++)
+    {
+        printf("arr[%d] = %d   \n",i,arr[i]);
+    }
+    free(arr);
+    arr = NULL;
+    return 0;
+
+}
